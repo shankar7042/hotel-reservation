2e040c572f2e309bb1944c039330c9cb2dcc9a1e
shankar7042 <shankar.kumar.4171@gmail.com>
2024-04-18 17:06:43 +0530

    custom error handler

--- main.go
+++ main.go
@@ -14,6 +14,14 @@ import (
 
 const dbUri = "mongodb://localhost:27017"
 
+var config = fiber.Config{
+	ErrorHandler: func(c fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{
+			"error": err.Error(),
+		})
+	},
+}
+
 func main() {
 
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbUri))
@@ -24,7 +32,7 @@ func main() {
 	listenAddr := flag.String("listenAddr", ":5000", "The listen address of the api server")
 	flag.Parse()
 
-	app := fiber.New()
+	app := fiber.New(config)
 	apiv1 := app.Group("/api/v1")
 	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
 	apiv1.Get("/user", userHandler.HandleGetUsers)
