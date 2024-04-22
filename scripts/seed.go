c08c65b0b7a408e5026b40d4a7e90d9ee3a18271
shankar7042 <shankar.kumar.4171@gmail.com>
2024-04-22 20:23:56 +0530

    added rating attribute in hotel

--- scripts/seed.go
+++ scripts/seed.go
@@ -18,11 +18,12 @@ var (
 	ctx        = context.Background()
 )
 
-func seedHotel(name, location string) {
+func seedHotel(name, location string, rating int) {
 	hotel := types.Hotel{
 		Name:     name,
 		Location: location,
 		Rooms:    []primitive.ObjectID{},
+		Rating:   rating,
 	}
 	rooms := []types.Room{
 		{
@@ -52,9 +53,9 @@ func seedHotel(name, location string) {
 }
 
 func main() {
-	seedHotel("Bellucia", "France")
-	seedHotel("The Oberoi Hotel", "India")
-	seedHotel("Lalit Hotel", "America")
+	seedHotel("Bellucia", "France", 3)
+	seedHotel("The Oberoi Hotel", "India", 4)
+	seedHotel("Lalit Hotel", "America", 1)
 }
 
 func init() {
